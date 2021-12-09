1fc538a0790c9ed969343dbf9938925ee3a50732
Aleksandar Yakimov <aleksandar.yakimov@sap.com>
2021-12-09 18:30:22 +0200

    Added Section3 classes

---
+++ Section3_Projects/src/RepetitionFun.java
@@ -0,0 +1,30 @@
+public class RepetitionFun {
+    public static void main(String[] args) {
+
+/*        int count = 0;
+
+        // WHILE Repetition Loop
+        while(count < 10) {
+            System.out.println(count);
+            count++;
+        } // end while
+*/
+
+/*        int count2 = 15;
+
+        // DO - WHILE Repetition Loop
+        do {
+            System.out.println(count2);
+            count2--;
+        }
+        while(count2 > 0);
+*/
+
+/*
+        // FOR Repetition Loop
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        } // end for
+*/
+    } // end main
+}

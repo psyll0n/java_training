439792a800417d59720d25ace8f06e57c344fd46
Aleksandar Yakimov <aleksandar.yakimov@sap.com>
2022-12-13 17:10:52 +0200

    Added FirstApp Java package example

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First line from app"); // This is a line comment
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+    }
+}
